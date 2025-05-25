e65c0440c8a259861838bed00d299a3c1af19d89
Tarboobot2888 <129686276+Tarboobot2888@users.noreply.github.com>
2025-05-25 03:41:48 +0300

    Update platform.ts

--- lib/webcrack/utils/platform.ts
+++ lib/webcrack/utils/platform.ts
@@ -1,3 +1,6 @@
 export function isBrowser(): boolean {
-  return typeof window !== 'undefined' || (typeof self !== 'undefined' && typeof self.importScripts === 'function');
+  return (
+    typeof window !== 'undefined' ||
+    (typeof self !== 'undefined' && typeof (self as any).importScripts === 'function')
+  );
 }
